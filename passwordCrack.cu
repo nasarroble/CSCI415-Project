c16f154444c8bd10b6ed547b586412f7ff35ba59
Nasar Roble <nroble@titan.cs.ndsu.nodak.edu>
2017-05-03 15:20:38 -0500

    updated

--- passwordCrack.cu
+++ passwordCrack.cu
@@ -49,50 +49,29 @@ do{
 }
 
 
-__global__ void parallel_passwordCrack(int length,int*d_output,int *a, long attempts )
+__global__ void parallel_passwordCrack(int length,int*d_output,int *a)
 {	
 	int idx = blockIdx.x*blockDim.x+threadIdx.x;
 	bool cracked = false;
-	int mark=0;
-        char alphabetTable[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };        
+
+  char alphabetTable[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };        
 	int newB[1000]; 
 
- // // randoms(states,alphabetTable,d_output);
 
- //  char alph ;//= 'a';
-// while(!cracked){
-//       alph =alphabetTable[rand()%26];
-//        d_output[idx] = int(alph);
-//      __syncthreads();
-//       for(int i = 0; i< length; i++){
-//         if(d_output[i] != a[i])
-//         {
-//           cracked = false;
 
-//         }
-//         else{
-//           cracked = true;
-//         }
-//       }
-//   }
-
-__shared__ int nIter;
+__shared__ int nIter ;
 __shared__ int idT;
+__shared__ long totalAttempt;
+
 
 
 do{
 
 
-//     newB[0]++;
-        
-//     if(mark<length){
-//         if (newB[idx] >= 26 + alphabetTable[idx]){ 
-//             newB[idx] -= 26; 
-//             newB[idx+1]++;
-//     }
-// }else{
-//         mark++;
-//     }
+  if(idx == 0){
+      nIter = 0;
+      totalAttempt = 0;
+    }
 
    newB[0]++;
     for(int i =0; i<length; i++){
@@ -108,51 +87,35 @@ do{
     {
         if(newB[k]!=a[k]){
             cracked=false;
-         //   nIter = 0;
             break;
         }else
         {
             cracked = true;
-           // nIter = 1;
-            // printf("idx:  %d  found\n", idx);
-            //  d_output[k] = newB[k];
-
         }
     }
-    if(cracked){
-      __syncthreads();
+    if(cracked && nIter == 0){
       idT = idx;
-      nIter = 1;
-       __syncthreads();
       break;
-
     }
-//    if( (tries & 0x7ffffff) == 0 )
-//        cout << "\r       \r   ";
-//    else if( (tries & 0x1ffffff) == 0 )
-//        cout << ".";
-    attempts++;
-}while(!cracked);
+    else if(nIter){
+      break;
+    }
 
-if(idx == idT){
+
+    totalAttempt++;
+}while(!cracked || !nIter);
+
+
+if(idx == idT)
+   {
         for(int i = 0; i< length; i++){
   
              d_output[i] = newB[i];
     }
 
+//printf("parallel Attempt: %d\n", totalAttempt);
+ }
 
-}
-
-//newB[idx];
-
-
-//    if( idx == 2 ){
-//          nIter =idx+1;
-//          printf("idx: %d: found, %d\n", idx, nIter);
-//        }
-// if(!nIter){
-//   printf("idx: %d: not found \n", idx);
-// }
 
 
 }
@@ -181,7 +144,6 @@ int main()
 {
 int length; //length of password
 int random; //random password to be generated
-long attempts = 0; //number of attempts to crack the password
 int *d_input = (int *) malloc(result);;
 
 
@@ -205,38 +167,34 @@ for (int i =0; i<length; i++){
 //allocate GPU memory
   cudaMalloc((void **) &d_output,1000*sizeof(int));
   cudaMalloc((void **) &d_input,result);
+
 //transfer the array to the GP
 
   cudaError_t err = cudaSuccess;
 err = cudaMemcpy(d_input, a,result,cudaMemcpyHostToDevice);
   if (err != cudaSuccess)
   {
-    fprintf(stderr, "Failed to copy d_S from host to device (error code %s)!\n", cudaGetErrorString(err));
+    fprintf(stderr, "Failed to copy random guess from host to device (error code %s)!\n", cudaGetErrorString(err));
       exit(EXIT_FAILURE);
   }
+
 //launch the kernel
-int threards = length*10;//(length*1000)/1024;
+int threards = length;
 
-
-   // for(int i = 0; i< length; i++){
-   //  printf("value: %d\n", d_input[i]);
-   // }
-//    
-
-/* CUDA's random number library uses curandState_t to keep track of the seed value
-     we will store a random state for every thread  */
-  curandState_t* states;
-
-  /* allocate space on the GPU for the random states */
-//  cudaMalloc((void**) &states, threards * sizeof(curandState_t));
-    /* invoke the GPU to initialize all of the random states */
-//   init<<<threards, threards>>>(time(0), states);
-
+long long parallel_start_time = start_timer();
 
 //parallel_passwordCrack<<<threards,1024>>>(length,d_output,d_input,attempts);
-parallel_passwordCrack<<<1,threards>>>(length,d_output,d_input,attempts);
+parallel_passwordCrack<<<1,threards>>>(length,d_output,d_input);
+
+long long parallel_end_time = stop_timer(parallel_start_time, "nParallel Run Time");
 //copy back the result array to the CPU
-cudaMemcpy(h_gpu_result,d_output,1000*sizeof(int),cudaMemcpyDeviceToHost);
+err = cudaMemcpy(h_gpu_result,d_output,1000*sizeof(int),cudaMemcpyDeviceToHost);
+
+if (err != cudaSuccess)
+  {
+    fprintf(stderr, "Failed to copy d_output from host to device (error code %s)!\n", cudaGetErrorString(err));
+      exit(EXIT_FAILURE);
+  }
 
 // cout << "Serial Password Cracked: " << endl;
 // serial_passwordCrack(length);
@@ -248,13 +206,12 @@ cudaMemcpy(h_gpu_result,d_output,1000*sizeof(int),cudaMemcpyDeviceToHost);
 cout << "\nParallel Password Cracked: " << endl;
 for(int i=0; i<length; i++){
 //	cout << char(h_gpu_result[i]);
-    printf("%c\n", char(h_gpu_result[i]));
+    printf("%c ", char(h_gpu_result[i]));
 }
-cout << "\nNumber of attempts: " << attempts << endl;
+printf("\n");
 
 cudaFree(d_output);
 cudaFree(d_input);
-cudaFree(states);
 free(h_gpu_result);
 
 return 0;
